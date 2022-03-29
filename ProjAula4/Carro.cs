71f0615a17b1fb098c865ed17bc624562c5800d9
= <andre.silva@hubdoctor.com.br>
2022-03-28 21:54:13 -0300

    V0

---
+++ ProjAula4/Carro.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjAula4
+{
+    public class Carro
+    {
+        #region Propriedades
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int AnoModelo { get; set; }
+        public int AnoFabricacao { get; set; }
+        public string Cor { get; set; }
+        public int Velocidade { get; set; }
+        public bool StatusCarro { get; set; }
+        public bool Buzinar { get; set; }
+        public bool Ar { get; set; }
+        #endregion
+
+        #region Métodos
+        //Acelera
+        public void Acelear(int velocidade)
+        {
+            this.Velocidade += velocidade;
+        }
+        //Frear
+        public void Frear(int velocidade)
+        {
+            this.Velocidade -= velocidade;
+        }
+        //Liga
+        public void Ligar()
+        {
+            this.StatusCarro = true;
+        }
+        //Desliga
+        public void Desligar()
+        {
+            this.StatusCarro = false;
+        }
+        //Buzina
+        public void LigarBuzina()
+        {
+            this.Buzinar = true;
+        }
+        
+        public void DesligarBuzina()
+        {
+            this.Buzinar = false;
+        }
+        //LigarAr
+        public void LigarAr()
+        {
+            if (this.Ar == false)
+            {
+                if (this.StatusCarro == true)
+                    this.Ar = true;
+                else
+                    Console.WriteLine("O carro está desligado!");
+            }
+            else
+            {
+                Console.WriteLine("O Ar já está ligado!");
+            }
+            
+        }
+        public void DesligarAr()
+        {
+            this.Ar = false;
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + this.Id +
+                   "\nModelo: " + this.Modelo +
+                   "\nMarca: " + this.Marca +
+                   "\nAno Modelo: " + this.AnoModelo +
+                   "\nAno Fabricação: " + this.AnoFabricacao +
+                   "\nCor: " + this.Cor +
+                   "\nStatus Carro: " + this.StatusCarro +
+                   "\nVelocidade: " + this.Velocidade +
+                   "\nBuzina: " + this.Buzinar + 
+                   "\nAr: " + this.Ar;
+        }
+        #endregion
+    }
+}
---
+++ ProjAula4/Program.cs
@@ -0,0 +1,27 @@
+﻿using ProjAula4;
+
+Carro carro = new Carro();
+
+carro.Id = 1;
+carro.Marca = "Fiat";
+carro.Modelo = "Toro";
+carro.AnoFabricacao = 2022;
+carro.AnoModelo = 2022;
+carro.Cor = "Preto";
+carro.Ligar();
+carro.Acelear(100);
+carro.LigarBuzina();
+carro.LigarAr();
+
+Carro carro2 = new Carro()
+{
+    Id = 2,
+    Marca = "Hyundai",
+    Modelo = "Creta",
+    AnoFabricacao = 2020,
+    AnoModelo = 2021,
+    Cor = "Branco"
+};
+
+Console.WriteLine(carro);
+Console.WriteLine(carro2);
